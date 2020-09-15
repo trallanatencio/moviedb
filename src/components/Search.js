95d49038798237d2f46d6a7b9229ad25da99e338
Allan <Allan@Macbooks-MacBook-Pro-2.local>
2020-09-15 10:37:47 +0800

    Working

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Search({ handleInput, search }) {
+    return (
+        <section className='searchbox-wrap'>
+            <input
+                type="text"
+                placeholder="Search for a Movie"
+                className="searchbox"
+                onChange={handleInput}
+                onKeyPress={search}
+            />
+
+        </section>
+    )
+}
+
+export default Search
